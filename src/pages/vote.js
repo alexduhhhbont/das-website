e03e763cfbc1b51c56b3fe096481d03623113dd6
Alex de Bont <alex.debont@vallonic.com>
2021-11-30 18:12:37 +0100

    test

--- src/pages/vote.js
+++ src/pages/vote.js
@@ -7,7 +7,7 @@ export default function Vote() {
 
     React.useEffect(() => {
         const id = setTimeout(() => {
-          setShouldRedirect(true);
+          setShouldRedirect(false);
         }, 4000);
     
         return () => clearTimeout(id);
@@ -20,7 +20,7 @@ export default function Vote() {
         )
     } 
     return (
-        <div style={{width: "100vw", height: "100vh", backgroundColor: "#95B6D2", margin: "0px", padding: "0px", overflow: "hidden"}}>
+        <div style={{width: "100vw", height: "100vh", backgroundColor: "#95B6D2", margin: "0 !important", padding: "0 !important", overflow: "hidden", position: "absolute", top: "0", left: "0"}}>
             <div style={{justifyContent: "center", display: "grid", padding: "90px"}}>
                 <div style={{width: "100%", height: "100%", display: "flex", justifyContent: "center"}}>
                     <img src={logo} alt="logo" style={{height: "100px"}}/>
